e79d0134b19e254077b230c653df8fe8e3238556
Nia <eugeniamashina@gmail.com>
2024-03-18 12:46:49 +0300

    fx

--- src/sections/how.jsx
+++ src/sections/how.jsx
@@ -125,7 +125,7 @@ export default function How() {
         await fetch("https://dev-h.rafinad.io/api/v1/create_landing_data/", {
           method: "POST",
           headers: {
-            "Content-Type": "application/json;charset=utf-8",
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(data),
         });
