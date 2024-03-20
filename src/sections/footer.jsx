808c972e3ca2430956aef30b8de061cf332fafae
Nia <eugeniamashina@gmail.com>
2024-03-20 11:30:57 +0300

    fx rules link

--- src/sections/footer.jsx
+++ src/sections/footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
           <a
             target="_blank"
             rel="noreferrer noopenner"
-            href="https://home.pfm.promo/pdf/%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%20%D0%BD%D0%B0%20%D0%BF%D1%80%D0%BE%D0%B2%D0%B5%D0%B4%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BA%D0%BE%D0%BD%D0%BA%D1%83%D1%80%D1%81%D0%B0.pdf"
+            href="https://rafinad.io/download-file/pravila-provedeniya-konkursa"
           >
             <p className="rules__header">Правила акции</p>
           </a>
